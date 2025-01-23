3ae9631c4f305dfb203ba56c698d3d7e412d2460
Vastayalover <cuteerin2002@gmail.com>
2025-01-22 21:29:02 -0300

    custom menu
    Hice testeos y pruebas de como personalizar el menu :P

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,14 +1,16 @@
 extends Control
 
+func _ready():
+	$MarginContainer/VBoxContainer/Play.grab_focus()
 
 func _on_play_pressed():
  # Cambiar a la escena del juego
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
-func _on_about_pressed():
+func _on_about_pressed(): #Boton de info
 	print("Todas putas")
 
 
-func _on_exit_pressed():
+func _on_exit_pressed(): #Boton de Salir
 	get_tree().quit()
