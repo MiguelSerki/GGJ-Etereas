7f521f52faf968cedecd501c75a890205945c580
Vastayalover <cuteerin2002@gmail.com>
2025-01-22 22:21:55 -0300

    Sobre nosotras
    Set-eando el apartado de informacion

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,12 +4,12 @@ func _ready():
 	$MarginContainer/VBoxContainer/Play.grab_focus()
 
 func _on_play_pressed():
- # Cambiar a la escena del juego
+ # Boton de inicio que cambia a la escena del juego
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_about_pressed(): #Boton de info
-	print("Todas putas")
+	get_tree().change_scene_to_file("res://scenes/sobre_nosotras.tscn")
 
 
 func _on_exit_pressed(): #Boton de Salir
