44d0160324ba6c02dc02937c36a829222592ee4d
MiguelSerki <miguel.serki@gmail.com>
2025-01-26 01:14:15 -0300

    Estilo Smarphone
    LO HIZO! LA MALDITA HIJA DE PERRA LO HIZO!

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_example_scene()
+	Dialogic.Styles.load_style('Smartphone_Style')
 	Dialogic.start("timeline1")
 	pass
 
@@ -10,3 +12,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func start_example_scene() -> void:
+	fade(true)
+
+## A fade in animation for this example
+func fade(fade_in:= false) -> void:
+	var tween := create_tween().set_parallel()
+	if fade_in:
+		self.modulate = Color.TRANSPARENT
+		tween.tween_property(self, 'modulate', Color.WHITE, 0.2)
+	else:
+		self.modulate = Color.WHITE
+		tween.tween_property(self, 'modulate', Color.TRANSPARENT, 0.2)
+	await tween.finished
+	await get_tree().create_timer(0.3).timeout
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func _on_play_pressed():
  # Boton de inicio que cambia a la escena del juego
+	await fade()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
@@ -19,3 +20,14 @@ func _on_about_pressed(): #Boton de info
 
 func _on_exit_pressed(): #Boton de Salir
 	get_tree().quit()
+
+func fade(fade_in:= false):
+	var tween := create_tween().set_parallel()
+	if fade_in:
+		self.modulate = Color.TRANSPARENT
+		tween.tween_property(self, 'modulate', Color.WHITE, 0.2)
+	else:
+		self.modulate = Color.WHITE
+		tween.tween_property(self, 'modulate', Color.TRANSPARENT, 0.2)
+	await tween.finished
+	await get_tree().create_timer(0.3).timeout
